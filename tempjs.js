6d9d4ea2df00c461997826ff0790c7f4e520f98b
unknown <ras0404@gmail.com>
2025-04-13 16:11:20 -0400

    pain

--- tempjs.js
+++ tempjs.js
@@ -1,3 +1,13 @@
+/*JS Audio Library (10 pts)
+You must implement the Use of an Audio Listening Library, the library is called Annyang
+On all pages the following 3 Options must be Implemented
+  Hello
+    Alerts with Hello World
+  Change the color to <color>
+    Changes the background color of the page to <color>
+  Navigate to <page>
+    Navigate to said page (hint you might need to lowercase to compare)
+*/
 function quoteJS() {
     fetch("https://zenquotes.io/api/quotes/")
       .then(response => response.json())
@@ -10,4 +20,19 @@ function quoteJS() {
       })
 }
   
-window.onload = quoteJS;
+function vopc(){
+  if (annyang) {
+    // Let's define a command.
+    const commands = {
+      'hello': () => { alert('Hello world!'); }
+    };
+  
+    // Add our commands to annyang
+    annyang.addCommands(commands);
+  
+    // Start listening.
+    annyang.start();
+  }
+  
+}
+window.onload = quoteJS, vopc;
