f9d53cc886810019b8ab15d628bf6d54397c6025
unknown <ras0404@gmail.com>
2025-04-13 22:31:11 -0400

    working on stock chart ig

--- tempjs.js
+++ tempjs.js
@@ -45,8 +45,6 @@ function vopc(){
   }
 }
   
-
-
 function start(){
   if (annyang){
     vopc();
@@ -61,4 +59,28 @@ function stop(){
     console.log("stopped")
   }
 }
+
+function chartFunc(){
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+}
+
 window.onload = quoteJS;
